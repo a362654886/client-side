a7f7a5c0db7127f3eaeb5a16631e02fa2146d544
Lei Lu <41553112+a362654886@users.noreply.github.com>
2022-04-10 01:15:22 +1200

    Update MainPage.tsx

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -87,9 +87,10 @@ const MainPage = (): JSX.Element => {
       history.location.pathname == "/mainPage" ||
       history.location.pathname.indexOf("/mainPage") == -1
     ) {
-      history.push({
+      /*history.push({
         pathname: "/mainPage/home",
-      });
+      });*/
+      window.location.href = "www.animepark.com/mainPage/home";
     } else {
       console.log(history.location.pathname);
     }
