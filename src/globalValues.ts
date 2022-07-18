876defb43207b6bdc70495744982f74b29308c44
Lei Lu <41553112+a362654886@users.noreply.github.com>
2022-07-18 21:35:24 +1200

    update

--- src/globalValues.ts
+++ src/globalValues.ts
@@ -1,4 +1,4 @@
-export const backEndLink ="https://buwf1faqv4.execute-api.us-east-1.amazonaws.com/dev/";
+export const backEndLink ="https://gmhjnxgnj3.execute-api.us-east-1.amazonaws.com/dev/";
 //export const backEndLink = "http://localhost:3000/dev/";
 
 export const windowLink =`https://www.animepark.com`;
